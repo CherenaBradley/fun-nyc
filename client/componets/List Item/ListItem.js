e2d5037a803bf8da9d93fa9b3c01f29fcc0ce3b4
CherenaBradley <Cherenanbradley@gmail.com>
2021-08-05 15:47:59 -0400

    spinning cards

--- client/componets/List Item/ListItem.js
+++ client/componets/List Item/ListItem.js
@@ -11,24 +11,16 @@ const ListItem = (props) => {
   return (
     
     <div className="Listcontainer">
-          <img className="Listcontainer__image" src={data.image_url} />
-      
-      <div className="Listcontainer__details">
-          <div className="Listcontainer__details--title">
-            <h3>{data.name}</h3>
-          </div>
-          <div className="Listcontainer__details--address">
-              <p1>Address: {data.location.display_address[0]} {data.location.display_address[1]}</p1>
-          </div>
-    
-            {data.phone == "" ? " ": <div className="Listcontainer__details--phone"> Phone: {data.display_phone}</div> }
+      <div className="ListCard">
+          <img className="ListCard__front" src={data.image_url} />
+
         
-          <div className="Listcontainer__details__rating">
-          <div class="stars-outer">
-                <div class="stars-inner" style={{width: `${newRating}%` }}></div>
-            </div>
-            <span className="Listcontainer__details__rating--number">{data.review_count}</span>
+
+        <div className="ListCard__back">
+          <p>I am the back</p>
+
         </div>
+
       </div>
     </div>
 
