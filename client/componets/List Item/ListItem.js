a57cd3e877fc2842c3490d8ec7ea17f030d9bf1c
CherenaBradley <Cherenanbradley@gmail.com>
2021-08-05 21:27:49 -0400

    Flip card css now

--- client/componets/List Item/ListItem.js
+++ client/componets/List Item/ListItem.js
@@ -6,18 +6,37 @@ const ListItem = (props) => {
     const rating = (data.rating/5)*100;
     const newRating = Math.round(rating/10)*10
     console.log(rating)
-    //const ratingPercent = `${Math.round(rating/10)*10}%`
     
   return (
     
     <div className="Listcontainer">
       <div className="ListCard">
-          <img className="ListCard__front" src={data.image_url} />
+        <div className="ListCard__front">
+        <h3 className="ListCard__front--title">{data.name}</h3>
+        <img className="ListCard__front" src={data.image_url} />
+        </div>
+         
 
         
 
         <div className="ListCard__back">
-          <p>I am the back</p>
+          <div className="ListCard__back__details">
+          <div className="ListCard__back__details--title">
+            <h3>{data.name}</h3>
+          </div>
+          <div className="ListCard__back__details--address">
+              <p1>Address: {data.location.display_address[0]} {data.location.display_address[1]}</p1>
+          </div>
+    
+            {data.phone == "" ? " ": <div className="ListCard__back__details--phone"> Phone: {data.display_phone}</div> }
+        
+          <div className="ListCard__back__details__rating">
+          <div class="stars-outer">
+                <div class="stars-inner" style={{width: `${newRating}%` }}></div>
+            </div>
+            <span className="ListCard__back__details__rating--number">{data.review_count}</span>
+        </div>
+      </div>
 
         </div>
 
